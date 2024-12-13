94e778ac35ad5bf2f6d35f19c4541dea99214e42
amird55 <amir.dgn@outlook.com>
2024-12-13 12:03:30 +0200

    בקורסים, כל ה-CRUD מול SQL עם פרומיס

--- Routers/course_R.js
+++ Routers/course_R.js
@@ -4,29 +4,34 @@ module.exports = router;
 
 let courses=[];
 
-router.post("/courses", (req, res) => { //Create - הוספה
+router.post("/courses",async (req, res) => { //Create - הוספה
     let course_name   = req.body.course_name;
 
     const Query = `INSERT INTO courses (name) VALUES('${course_name}')`;
     // console.log(Query);
-    db_pool.query(Query,function (err,rows,fields,){
-        if (err){
-            res.status(500).json({message:err});
-        }else{
-            res.status(200).json({message:"OK",Last_Id:rows.insertId});
-        }
-    })
+    const promisePool = db_pool.promise();
+    let rows=[];
+    try {
+        [rows] = await promisePool.query(Query);
+        res.status(200).json({msg:"ok",data:rows,Last_Id:rows.insertId});
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({message: err});
+    }
 });
-router.get('/courses', (req, res) => { //Read - קבלת רשימה
+router.get('/courses',async (req, res) => { //Read - קבלת רשימה
     const Query = `SELECT * FROM courses `;
     // console.log(Query);
-    db_pool.query(Query,function (err,rows,fields,){
-        if (err){
-            res.status(500).json({message:err});
-        }else{
-            res.status(200).json(rows);
-        }
-    })
+    const promisePool = db_pool.promise();
+    let rows=[];
+    try {
+        [rows] = await promisePool.query(Query);
+        res.status(200).json({msg:"ok",data:rows});
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({message: err});
+    }
+
 });
 router.put('/courses', async (req, res) => { //Update - עריכה
     let idx             = req.body.idx;
@@ -46,8 +51,18 @@ router.put('/courses', async (req, res) => { //Update - עריכה
         return res.status(500).json({message: err});
     }
 });
-router.delete('/courses', (req, res) => { // Delete - מחיקה
+router.delete('/courses',async (req, res) => { // Delete - מחיקה
     let idx             = req.body.idx;
-    courses.splice(idx, 1);
-    res.status(200).json("ok");
+    let Query = `DELETE FROM courses  `;
+    Query += ` WHERE id = ${idx} `;
+
+    const promisePool = db_pool.promise();
+    let rows=[];
+    try {
+        [rows] = await promisePool.query(Query);
+        res.status(200).json({msg:"ok",data:rows});
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({message: err});
+    }
 });
