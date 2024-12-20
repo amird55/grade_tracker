7f6b8d21968b5ccee7b659e7f92e3f7564ca34d2
amird55 <amir.dgn@outlook.com>
2024-12-20 13:23:44 +0200

    הוספת קריאה וחיפוש של סטודנטים

--- middleware/Student_Mid.js
+++ middleware/Student_Mid.js
@@ -19,7 +19,32 @@ async function AddStudent(req, res,next) {
     }
     next();
 }
+async function ReadStudents(req,res,next){
+    let search_str = (req.body.search_txt===undefined) ? "" : req.body.search_txt;
 
-export {
+    let Query = `SELECT * FROM student `;
+    if(search_str !== ""){
+        Query += ` WHERE (name LIKE '%${search_str}%')`;
+        Query += ` OR (tz LIKE '%${search_str}%')`;
+    }
+    // console.log(Query);
+    const promisePool = db_pool.promise();
+    let rows=[];
+    try {
+        [rows] = await promisePool.query(Query);
+        for(let idx in rows){
+            rows[idx].name= htmlspecialchars(rows[idx].name);
+            rows[idx].tz  = htmlspecialchars(rows[idx].tz);
+        }
+        req.success=true;
+        req.students_data=rows;
+    } catch (err) {
+        req.success=false;
+        console.log(err);
+    }
+    next();
+}
+module.exports = {
     AddStudent,
+    ReadStudents,
 }
